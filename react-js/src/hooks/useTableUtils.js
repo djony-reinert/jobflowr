11dcc320e68b4459969822952abea96328ab705f
Djony Reinert <djony.reinert@jobscore.com>
2023-04-17 02:04:02 -0300

    add linter configuration and apply lint-fix

--- react-js/src/hooks/useTableUtils.js
+++ react-js/src/hooks/useTableUtils.js
@@ -1,6 +1,6 @@
-import {useEffect, useState, useCallback} from "react";
+import { useEffect, useState, useCallback } from "react";
 
-const DEFAULT_ORDER = 'asc';
+const DEFAULT_ORDER = "asc";
 const DEFAULT_ROWS_PER_PAGE = 5;
 
 const descendingComparator = (a, b, orderBy) => {
@@ -11,14 +11,13 @@ const descendingComparator = (a, b, orderBy) => {
     return 1;
   }
   return 0;
-}
+};
 
 const getComparator = (order, orderBy) => {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
+};
 
 const stableSort = (array, comparator) => {
   if (!array) return;
@@ -32,18 +31,18 @@ const stableSort = (array, comparator) => {
     return a[1] - b[1];
   });
   return stabilizedThis.map((el) => el[0]);
-}
+};
 
 const getInitialVisibleRows = (data, orderBy) => {
   let rowsOnMount = stableSort(data, getComparator(DEFAULT_ORDER, orderBy));
 
   rowsOnMount = rowsOnMount?.slice(
     0 * DEFAULT_ROWS_PER_PAGE,
-    0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE,
+    0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE
   );
 
   return rowsOnMount;
-}
+};
 export const useTableUtils = ({ columns, data }) => {
   const [visibleRows, setVisibleRows] = useState([]);
   const [order, setOrder] = useState(DEFAULT_ORDER);
@@ -56,46 +55,58 @@ export const useTableUtils = ({ columns, data }) => {
     setVisibleRows(initialVisibleRows);
   }, [data]);
 
-  const handleRequestSort = useCallback((event, newOrderBy) => {
-    const isAsc = orderBy === newOrderBy && order === 'asc';
-    const toggledOrder = isAsc ? 'desc' : 'asc';
-    setOrder(toggledOrder);
-    setOrderBy(newOrderBy);
+  const handleRequestSort = useCallback(
+    (event, newOrderBy) => {
+      const isAsc = orderBy === newOrderBy && order === "asc";
+      const toggledOrder = isAsc ? "desc" : "asc";
+      setOrder(toggledOrder);
+      setOrderBy(newOrderBy);
 
-    const sortedRows = stableSort(data, getComparator(toggledOrder, newOrderBy));
-    const updatedRows = sortedRows.slice(
-      page * rowsPerPage,
-      page * rowsPerPage + rowsPerPage,
-    );
+      const sortedRows = stableSort(
+        data,
+        getComparator(toggledOrder, newOrderBy)
+      );
+      const updatedRows = sortedRows.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      );
 
-    setVisibleRows(updatedRows);
-  }, [orderBy, order, data, page, rowsPerPage]);
+      setVisibleRows(updatedRows);
+    },
+    [orderBy, order, data, page, rowsPerPage]
+  );
 
-  const handleChangePage = useCallback((_, newPage) => {
-    setPage(newPage);
+  const handleChangePage = useCallback(
+    (_, newPage) => {
+      setPage(newPage);
 
-    const sortedRows = stableSort(data, getComparator(order, orderBy));
-    const updatedRows = sortedRows.slice(
-      newPage * rowsPerPage,
-      newPage * rowsPerPage + rowsPerPage
-    );
+      const sortedRows = stableSort(data, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        newPage * rowsPerPage,
+        newPage * rowsPerPage + rowsPerPage
+      );
 
-    setVisibleRows(updatedRows);
-  }, [data, order, orderBy, rowsPerPage]);
+      setVisibleRows(updatedRows);
+    },
+    [data, order, orderBy, rowsPerPage]
+  );
 
-  const handleChangeRowsPerPage = useCallback((event) => {
-    const updatedRowsPerPage = event.target.value;
-    setRowsPerPage(updatedRowsPerPage);
-    setPage(0);
+  const handleChangeRowsPerPage = useCallback(
+    (event) => {
+      const updatedRowsPerPage = event.target.value;
+      setRowsPerPage(updatedRowsPerPage);
+      setPage(0);
 
-    const sortedRows = stableSort(data, getComparator(order, orderBy));
-    const updatedRows = sortedRows.slice(
-      0 * updatedRowsPerPage,
-      0 * updatedRowsPerPage + updatedRowsPerPage
-    );
+      const sortedRows = stableSort(data, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        0 * updatedRowsPerPage,
+        0 * updatedRowsPerPage + updatedRowsPerPage
+      );
 
-    setVisibleRows(updatedRows);
-  }, [data, order, orderBy]);
+      setVisibleRows(updatedRows);
+    },
+    [data, order, orderBy]
+  );
 
   return {
     visibleRows,
@@ -108,6 +119,6 @@ export const useTableUtils = ({ columns, data }) => {
     setPage,
     handleChangePage,
     handleChangeRowsPerPage,
-    handleRequestSort
+    handleRequestSort,
   };
 };
